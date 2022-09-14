ecf69a238e23b33ae5027f6d2bdb45afa91f0b5f
LeRoy-M <lmwasela@gmail.com>
2022-09-14 11:31:36 -0700

    9. ByteCode -> Python #4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-from dis import dis
 def magic_calculation(a, b):
-    for i in range(x):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('too far')
+
+            result += a ** b / i
+        except:
+            result = b + a
+        break
+    return result

3ac8b11b0d1d52beb12916492ed1e21d426f99e6
LeRoy-M <lmwasela@gmail.com>
2022-09-20 09:16:41 -0700

    0. Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Takes two integers as input and adds them together.
+    Args:
+        a (int/ float): First Input
+        b (int/ float): Second Input
+"""
+
+def add_integer(a, b=98):
+    """Adds two integers.
+        Returns a + b
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a + b)

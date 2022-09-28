df85b33dfd44495d7f005e22dc23bb0849d23e50
LeRoy-M <lmwasela@gmail.com>
2022-09-28 09:35:48 -0700

    1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module creates a class that inherits from 'list'"""
+
+
+class MyList(list):
+    """This class inherits from list"""
+    def ___init__(self):
+        """Initialization method"""
+        super().__init__()
+
+    def print_sorted(self):
+        """This method prints a sorted list"""
+        print (sorted(self))

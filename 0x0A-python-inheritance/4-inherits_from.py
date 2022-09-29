5240dbf296124c70d31e8d648b9c15ccde495286
LeRoy-M <lmwasela@gmail.com>
2022-09-29 10:39:21 -0700

    4. Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module contains one function"""
 
+
 def inherits_from(obj, a_class):
-    """This function checks if an object is an instance of a 
+    """This function checks if an object is an instance of a
     class that inherited from the specified class
     """
     return (isinstance(obj, a_class) and type(obj) != a_class)

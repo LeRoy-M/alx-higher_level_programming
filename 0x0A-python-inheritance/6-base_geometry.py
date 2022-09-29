e4c7b78a97ff91a2f3a5ee83448e260cbd3346a0
LeRoy-M <lmwasela@gmail.com>
2022-09-29 10:57:39 -0700

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module creates a class"""
+
+
+class BaseGeometry:
+    """This class is based on the previous module"""
+    def area(self):
+        """This method raises an exception"""
+        raise Exception("area() is not implemented")

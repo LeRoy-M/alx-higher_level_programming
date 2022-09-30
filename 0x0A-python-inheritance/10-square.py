fa93726d1b13ed6f7068a035b8ab0dc99a5025b3
LeRoy-M <lmwasela@gmail.com>
2022-09-29 23:59:25 -0700

    10. Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module creates a class 'Square'"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This class is based on the previous module (9-rectangle.py)"""
+    def __init__(self, size):
+        """Initialization method that validates dimensions passed
+        to it through inheritance of 'BaseGeometry' class
+
+        Args:
+            size (int): Value passed in
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """This method computes a square's area"""
+        return self.__size ** 2

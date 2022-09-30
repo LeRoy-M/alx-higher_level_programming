b6f1a280eb784013113522ffb8729a5b90ac3e53
LeRoy-M <lmwasela@gmail.com>
2022-09-30 00:38:49 -0700

    11. Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module creates a class 'Square'"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This class is based on the previous module (10-square.py)"""
+    def __init__(self, size):
+        """Initialization method that validates dimensions passed
+        to it through inheritance of 'Rectangle' class
+
+        Args:
+            size (int): Value passed in
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """This method computes a square's area"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """This method prints out a string"""
+        return f"[Square] {self.__size}/{self.__size}"

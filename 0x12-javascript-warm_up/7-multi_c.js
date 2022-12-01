21458f91a41d9d604e1a2bace4993c78802d7370
LeRoy-M <lmwasela@gmail.com>
2022-12-01 16:24:46 +0000

    7. I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,10 @@ let i = 0;
 
 if (process.argv[2] === undefined) {
   console.log('Missing number of occurrences');
+} else if (process.argv[2] < i) {
+  process.stdout.write('');
 } else {
-  while (i != process.argv[2]) {
+  for (; i < process.argv[2]; i++) {
     console.log('C is fun');
-    i++;
   }
 }

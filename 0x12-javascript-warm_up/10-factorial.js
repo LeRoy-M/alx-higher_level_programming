0056e471b48b2455e9ab428a0f123c503dcc72c9
LeRoy-M <lmwasela@gmail.com>
2022-12-05 15:34:30 +0000

    10. Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let f = parseInt(process.argv[2]);
+
+function factorial(f) {
+  if (isNaN(f) || f === undefined || f === 0) {
+    return 1;
+  }
+
+  return f * factorial(f - 1);
+}
+
+console.log(factorial(f));

c9813cf54ab6716ecd5020bde4dec3370ca4e1f6
LeRoy-M <lmwasela@gmail.com>
2023-05-17 18:09:26 +0300

    10. Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None: return None
+    key = ""
+    keys = list(a_dictionary.keys())
+    for i in range(len(keys)):
+        if i == len(keys) - 1:
+            break
+        elif (a_dictionary[keys[i]] > a_dictionary[keys[i + 1]]):
+            key = keys[i]
+        else:
+            key = keys[i + 1]
+
+    return (key)

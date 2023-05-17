66efdb756d1f5e77d5b0dccf90b0fcf6633ae998
LeRoy-M <lmwasela@gmail.com>
2023-05-17 18:11:07 +0300

    10. Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None: return None
+    if a_dictionary is None:
+        return None
     key = ""
     keys = list(a_dictionary.keys())
     for i in range(len(keys)):

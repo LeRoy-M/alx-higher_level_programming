a5cf0b1d7fca71a354f7c7dd909b221474888982
LeRoy-M <lmwasela@gmail.com>
2023-05-17 18:10:12 +0300

    10. Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,7 +7,7 @@ def best_score(a_dictionary):
     for i in range(len(keys)):
         if i == len(keys) - 1:
             break
-        elif (a_dictionary[keys[i]] > a_dictionary[keys[i + 1]]):
+        if (a_dictionary[keys[i]] > a_dictionary[keys[i + 1]]):
             key = keys[i]
         else:
             key = keys[i + 1]

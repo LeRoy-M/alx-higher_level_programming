3607773632d9a91cdb08bf60a21131b5e3414753
LeRoy-M <lmwasela@gmail.com>
2023-05-17 19:03:54 +0300

    12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,21 +4,24 @@ def roman_to_int(roman_string):
     I, V, X, L, C, D, M, num = 1, 5, 10, 50, 100, 500, 1_000, 0
     if (roman_string is None or not isinstance(roman_string, str)):
         return (0)
-    for rn in roman_string:
-        if rn == "M":
+    for rch in roman_string:
+        if rch == "M":
             num += M
-        elif rn == "D":
+        elif rch == "D":
             num += D
-        elif rn == "C":
+        elif rch == "C":
             num += C
-        elif rn == "L":
+        elif rch == "L":
             num += L
-        elif rn == "X":
+        elif rch == "X":
             num += X
-        elif rn == "V":
+        elif rch == "V":
             num += V
-        elif rn == "I":
-            num += I
+        elif rch == "I":
+            if rch is roman_string[-1]:
+                num += I
+            else:
+                num -= I
         else:
             continue
     return (num)

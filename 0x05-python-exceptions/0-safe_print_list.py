dec3233b5e858d8d06f7d5d4b5dcdfd47909e322
LeRoy-M <lmwasela@gmail.com>
2023-05-29 12:25:19 +0300

    0. Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    if my_list:
+        for i in range(x):
+            try:
+                if i < (x - 1): print("{:d}".format(my_list[i]), end="")
+                else: print("{:d}".format( my_list[i]))
+            except IndexError:
+                print("")
+                i -= 1
+                break
+    return (i+1)

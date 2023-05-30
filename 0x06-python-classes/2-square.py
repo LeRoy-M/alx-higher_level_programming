2099ab78f881492ffcdf9e0c65dd194e0b53912a
LeRoy-M <lmwasela@gmail.com>
2023-05-30 17:55:46 +0300

    2. Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module defines a class 'Square' based off of
+the module '1-square'
+"""
+
+
+class Square:
+    """The class 'Square' that has an private
+    attribute
+    """
+    def __init__(self, size=0):
+        """The class is initialized here, with
+        'size' attribute
+        """
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")

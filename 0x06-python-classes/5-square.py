71e69d75adf4fe9c2581ac28926f15d5bbc71443
LeRoy-M <lmwasela@gmail.com>
2023-05-30 21:10:20 +0300

    5. Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""This module defines a class 'Square' based off of
+the module '4-square'
+"""
+
+
+class Square:
+    """The class 'Square' that has a private
+    attribute
+    """
+    def __init__(self, size=0):
+        """The class is initialized here"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter method to retrieve 'size' field"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Sets the value of an object variable 'size'"""
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Calculates the area of a square object and
+        returns the value
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Prints in stdout the area of a square object and
+        returns the value
+        """
+        if self.__size == 0:
+            print("")
+        for i in range(self.__size):
+            for i in range(self.__size):
+                print("#", end="")
+            print("")

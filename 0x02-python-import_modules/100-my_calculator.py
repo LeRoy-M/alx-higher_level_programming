5c44a7d8557bc3438539886b3ccb0dad98ca0b18
LeRoy-M <lmwasela@gmail.com>
2023-05-31 11:31:28 +0300

    6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
-#if __name__ != "__main__":
-    #exit()
+if __name__ != "__main__":
+    exit()
 
 if len(argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")

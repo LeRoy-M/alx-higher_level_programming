4b1f7313413ba6fee2c5cf1b9a88b8d15fbe12a6
LeRoy-M <lmwasela@gmail.com>
2023-06-05 17:21:20 +0300

    6. How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -30,8 +30,8 @@ class Rectangle:
 
     def __del__(self):
         """Method to delete an instance of the 'Rectangle' class"""
-        print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
 
     @property
     def width(self):

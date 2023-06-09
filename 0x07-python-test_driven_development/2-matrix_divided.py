49ee043e07aaa282ad6f0ae350451cc5423574e8
LeRoy-M <lmwasela@gmail.com>
2023-06-09 17:00:38 +0300

    1. Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,13 +3,15 @@
 
 
 def matrix_divided(matrix, div):
+    if len(matrix[0]) != len(matrix[1]):
+        raise TypeError("Each row of the matrix must have the same size")
     new_mat = [[] for i in range(2)]
     """Function for matrix division"""
     for x in range(len(matrix)):
         for y in range(len(matrix[x])):
-            if not isinstance(matrix[x][y], int) and
-            not isinstance(matrix[x][y], float):
-                raise TypeError("matrix must be a matrix
-                                (list of lists) of integers/floats")
+            if not isinstance(matrix[x][y], int) and \
+               not isinstance(matrix[x][y], float):
+                raise TypeError("matrix must be a matrix" +
+                                " (list of lists) of integers/floats")
             new_mat[x].append(round(matrix[x][y] / div, 2))
     return new_mat

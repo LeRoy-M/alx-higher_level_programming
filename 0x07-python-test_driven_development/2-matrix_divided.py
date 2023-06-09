44396cb1a74c85d7c8c20e9896cda0a5fddbaad9
LeRoy-M <lmwasela@gmail.com>
2023-06-09 16:24:00 +0300

    1. Divide a matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module contains a function that divides all elements of a matrix"""
+
+
+def matrix_divided(matrix, div):
+    new_mat = [[] for i in range(2)]
+    """Function for matrix division"""
+    for x in range(len(matrix)):
+        for y in range(len(matrix[x])):
+            if not isinstance(matrix[x][y], int) and not isinstance(matrix[x][y], float):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            new_mat[x].append(round(matrix[x][y] / div, 2))
+    return new_mat

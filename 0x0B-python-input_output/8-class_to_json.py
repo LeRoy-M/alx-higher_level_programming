252bbeb505c79af5924ba16d1a26eed6869a078d
LeRoy-M <lmwasela@gmail.com>
2023-06-13 21:45:31 +0300

    8. Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains the 'class_to_json' function"""
+import json
+
+
+def class_to_json(obj):
+    """Function that returns dict description with simple
+    data structure for JSON serialization of an object
+    """
+    return json.loads(json.dumps(obj, default=lambda o: o.__dict__))

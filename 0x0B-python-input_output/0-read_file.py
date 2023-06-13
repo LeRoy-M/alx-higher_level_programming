720082a238a9ea0badc13433d40297d636c817fe
LeRoy-M <lmwasela@gmail.com>
2023-06-13 16:29:43 +0300

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """Function that reads a text file and
     prints it to stdout
     """
-    with open(filename, mode="r", encoding="UTF-8") as fn:
+    with open(filename, mode="r", encoding="utf-8") as fn:
         print(fn.read(), end="")

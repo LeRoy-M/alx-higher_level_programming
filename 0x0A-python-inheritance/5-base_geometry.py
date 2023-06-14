6bf97bbd62c8669fd50f898081004d5f8eeb1600
LeRoy-M <lmwasela@gmail.com>
2023-06-14 22:53:07 +0300

    5. Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains the 'BaseGeometry' class"""
+
+
+class BaseGeometry:
+    """Class definition for 'BaseGeometry'"""
+    def __init__(self):
+        """Initialization method"""
+        pass

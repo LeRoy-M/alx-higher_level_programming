a8dbbf70aa862296b512fbddf28dd0d880a87ee1
LeRoy-M <lmwasela@gmail.com>
2023-06-15 01:47:26 +0300

    10. Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module contains the 'Square' class and inherits
+from 'Rectangle' in module '9-rectangle'
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class definition for 'Square'"""
+    def __init__(self, size):
+        """Initialization method"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """String method"""
+        return f"[{super().__class__.__name__}] {self.__size}/{self.__size}"
+
+    def area(self):
+        """Method to calculate area of a square"""
+        return self.__size * self.__size

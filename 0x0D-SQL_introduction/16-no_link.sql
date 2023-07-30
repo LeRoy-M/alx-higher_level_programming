2e42e527f895740fcf6204ec5298cabe05f342d0
LeRoy-M <lmwasela@gmail.com>
2023-07-30 09:44:17 +0300

    16. Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
--- Script that lists all records of the table 'second_table' of the database 'hbtn_0c_0' in ones MySQL server
+-- Script that lists all records of the table 'second_table' of the database 'hbtn_0c_0' in a MySQL server
 SELECT score, name FROM second_table WHERE name IS NOT NULL AND name!='' ORDER BY score DESC;

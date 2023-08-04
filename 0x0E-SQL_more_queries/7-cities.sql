66f335f25551d0b6728eac92b7c5a0f1bf8297e0
LeRoy-M <lmwasela@gmail.com>
2023-08-04 15:26:47 +0300

    7. Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,7 @@
 -- Script that creates a database 'hbtn_0d_usa' and a table 'cities' on a MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
 	id INT AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
 	state_id INT FOREIGN KEY NOT NULL,
 	name VARCHAR(256) NOT NULL,

023b4816502536b8e10147a0e8266603f63164f8
LeRoy-M <lmwasela@gmail.com>
2023-08-14 16:47:02 +0300

    2. Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv.length;
+
+if (args > 2) {
+  console.log('Arguments found');
+} else if (args === 2) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}

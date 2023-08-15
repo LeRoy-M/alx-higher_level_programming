6d623a0d15468e56a921acebd94f88b30e21b756
LeRoy-M <lmwasela@gmail.com>
2023-08-15 13:06:56 +0300

    1. Rectangle #1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
-  width;
-  height;
-
-  constructor (width, height) {
-    this.width = width;
-    this.height = height;
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
 }
 

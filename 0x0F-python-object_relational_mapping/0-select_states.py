331330e230710fccefb4b5a048b220d0773dcb24
LeRoy-M <lmwasela@gmail.com>
2023-08-18 13:01:59 +0300

    0. Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that uses 'MySQLdb' import to lists all 'states' in
+'hbtn_0e_0_usa' database
+"""
+import MySQLdb
+db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+
+print(states)

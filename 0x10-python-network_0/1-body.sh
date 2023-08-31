f72b2ba6936942afc07fe714b28150e0890c86f0
LeRoy-M <lmwasela@gmail.com>
2023-08-31 18:35:31 +0300

    1. cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Script that displays the body of a URL response using 'curl'
-code=$(curl -so  /dev/null -Iw "%{http_code}" "$1")
-
-if [ "$code" -eq 200 ]; then
-	curl -sL "$1"
-fi
+curl -sL "$1"

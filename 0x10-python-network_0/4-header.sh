c0c0810a6433046bb1143d7befe93bc1f18a8214
LeRoy-M <lmwasela@gmail.com>
2023-08-31 22:50:53 +0300

    4. cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script that displays the body of a URL response with a header
-# variable 'X-School-User-Id' which has value '98' using 'curl'
-curl -sH "X-School-User-Id: 98" "$1"
+# Script that displays the body of a URL response with a header variable 'X-School-User-Id' which has value '98' using 'curl'
+curl -H "X-School-User-Id: 98" -sLX GET "$1"

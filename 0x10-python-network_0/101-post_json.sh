230f140a1a587529f205526b7961fdade35c1353
LeRoy-M <lmwasela@gmail.com>
2023-09-01 01:43:34 +0300

    8. cURL a JSON file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a JSON 'POST' request and displays the body of a URL response using 'curl'
-curl -sLX POST "$2" -H "Content-Type: application/json" -d "$(cat "$1")"
+curl -sLX POST -H "Content-Type: application/json" -d "$2" "$(cat "$1")"

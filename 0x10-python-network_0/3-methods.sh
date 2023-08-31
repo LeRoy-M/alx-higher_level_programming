9b4893ff9c80bab2adb29bb21f1613579623e327
LeRoy-M <lmwasela@gmail.com>
2023-08-31 22:19:39 +0300

    3. cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that displays all HTTP methods a server accepts 'curl'
-curl -vX "$1" | awk "/Allow/ { print $2 }" | cut -d " " -f 2
+curl -sI "$1" | awk "/Allow/ { print $2 }" | cut -d " " -f 2

8d6dbb2eadbc35c0c3c0cc0de7f1900861f41000
LeRoy-M <lmwasela@gmail.com>
2023-09-01 23:17:55 +0300

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,8 +3,13 @@
 with the email as a parameter, and displays the body of the response
 """
 import urllib.request as UR
+import urllib.parse as UP
 from sys import argv
 
 if __name__ == "__main__":
-    with UR.urlopen(f"argv[1]?email=argv[2]") as response:
-        print(reponse)
+    url = argv[1]
+    val = {"email": argv[2]}
+    data = UP.urlencode(val).encode("ascii")
+    request = UR.Request(url, data)
+    with UR.urlopen(request) as response:
+        print(reponse.read().decode("utf8"))

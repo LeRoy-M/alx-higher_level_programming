f261914c1173700ae301a3b2dfec43abc6f24382
LeRoy-M <lmwasela@gmail.com>
2023-09-02 22:33:03 +0300

    8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     q = ""
     if len(argv) > 1:
         q = argv[1]
-    response = requests.post("http://0.0.0.0:5000/search_user", {"q": q})
+    response = requests.get("http://0.0.0.0:5000/search_user", {"q": q})
     print(response.text)

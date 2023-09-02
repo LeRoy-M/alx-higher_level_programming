822eae40d3139f751191f0f8df3a9a590df4f429
LeRoy-M <lmwasela@gmail.com>
2023-09-02 18:47:11 +0300

    4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests
 if __name__ == "__main__":
     response = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type:", type(str(response)))
+    print("\t- type:", type(response.text))
     print("\t- content:", response.text)

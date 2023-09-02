53594f3ac0cb599f120c14d51bdd3360cd99533e
LeRoy-M <lmwasela@gmail.com>
2023-09-02 23:34:46 +0300

    9. My GitHub!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that takes ones GitHub credentials (username & password) and uses
+the GitHub API to display their 'id'
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    api_r = requests.get(url, auth=(argv[1], argv[2])).json()
+    print(f"{api_r['id']}")

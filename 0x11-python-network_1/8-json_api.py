aa4fa25c4ac913b18fb2e92d64a5102238f2f255
LeRoy-M <lmwasela@gmail.com>
2023-09-02 22:30:07 +0300

    8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that takes a letter and sends a 'POST' request to
+'http://0.0.0.0:5000/search_user' with the letter as
+a parameter
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    q = ""
+    if len(argv) > 1:
+        q = argv[1]
+    response = requests.post("http://0.0.0.0:5000/search_user", {"q": q})
+    print(response.text)

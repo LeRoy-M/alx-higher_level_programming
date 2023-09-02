29e6223fd8738a22e754dbecf98cd266464ffdb4
LeRoy-M <lmwasela@gmail.com>
2023-09-02 18:29:42 +0300

    3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Module that takes a URL and displays the body of the response"""
-from urllib import request, error
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
 from sys import argv
 
 if __name__ == "__main__":
-    # request = request.Request(argv[1])
+    request = Request(argv[1])
 
-    # with request.urlopen(request) as response:
-    with request.urlopen(argv[1]) as response:
-        try:
+    try:
+        with urlopen(request) as response:
             print(response.read().decode("utf-8"))
-        except error.HTTPError as e:
-            print(f"Error code: {e.code}")
+    except HTTPError as e:
+        print(f"Error code: {e.code}")

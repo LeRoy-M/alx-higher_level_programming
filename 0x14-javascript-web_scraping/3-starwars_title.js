e555a9c916a63f86c36ee28f53622f48dbc85d45
LeRoy-M <lmwasela@gmail.com>
2023-09-26 06:23:00 -0700

    3. Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const request = require('request');
+
+request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (error, response, body) => {
+  if (error) console.log(error);
+  console.log(body.title);
+});

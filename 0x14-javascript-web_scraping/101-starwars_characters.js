08bc4f53759fc66f4d9f526d930a5d239dc9427e
LeRoy-M <lmwasela@gmail.com>
2023-09-27 02:00:09 -0700

    8. Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,12 +4,13 @@ const request = require('request');
 request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (error, response, body) => {
   const parsed = JSON.parse(body).characters;
   if (error) console.log(error);
-
-  for (let i = 0; i < parsed.length; i++) {
-    request(parsed[i], (error, response, body) => {
-      const character = JSON.parse(body).name;
-      if (error) console.log(error);
-      console.log(character);
-    });
-  }
+  logName(parsed, 0);
 });
+
+function logName (parsed, idx) {
+  request(parsed[idx], (error, response, body) => {
+    if (error) console.log(error);
+    console.log(JSON.parse(body).name);
+    if ((idx + 1) < parsed.length) { logName(parsed, ++idx); }
+  });
+}
